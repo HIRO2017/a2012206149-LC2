e5c4d995c95a53789dd2ee328633497cf0f4cea5
Hiroshi Miyasato Reyes <hiroshi_miyasato@usmp.pe>
2017-06-10 18:45:37 -0500

    controladres configurados

---
+++ a12206149-SLN/a12206149.MVC/Controllers/BusesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using a12206149_ENT.Entities;
+using a12206149_PER;
+using a12206149_ENT.IRepositories;
+
+namespace a12206149.MVC.Controllers
+{
+    public class BusesController : Controller
+    {
+        //private a12206149DbContext db = new a12206149DbContext();
+
+        private readonly IUnityOfWork _UnityOfWork;
+
+
+        public BusesController(IUnityOfWork unityOfWork)
+        {
+            _UnityOfWork = unityOfWork;
+        }
+        // GET: Buses
+        public ActionResult Index()
+        {
+            return View(_UnityOfWork.Buses.GetAll());
+        }
+
+        // GET: Buses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bus bus = _UnityOfWork.Buses.Get(id);
+            if (bus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bus);
+        }
+
+        // GET: Buses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Buses/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "busId,descripcion,destigno,capacidad")] Bus bus)
+        {
+            if (ModelState.IsValid)
+            {
+                _UnityOfWork.Buses.Add(bus);
+                _UnityOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bus);
+        }
+
+        // GET: Buses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bus bus = _UnityOfWork.Buses.Get(id);
+            if (bus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bus);
+        }
+
+        // POST: Buses/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "busId,descripcion,destigno,capacidad")] Bus bus)
+        {
+            if (ModelState.IsValid)
+            {
+                _UnityOfWork.StateModified(bus);
+                _UnityOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bus);
+        }
+
+        // GET: Buses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bus bus = _UnityOfWork.Buses.Get(id);
+            if (bus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bus);
+        }
+
+        // POST: Buses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bus bus = _UnityOfWork.Buses.Get(id);
+            _UnityOfWork.Buses.Remove(bus);
+            _UnityOfWork.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _UnityOfWork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ a12206149-SLN/a12206149.MVC/Controllers/EmpleadoesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using a12206149_ENT.Entities;
+using a12206149_PER;
+using a12206149_ENT.IRepositories;
+
+namespace a12206149.MVC.Controllers
+{
+    public class EmpleadoesController : Controller
+    {
+        //private a12206149DbContext db = new a12206149DbContext();
+        private readonly IUnityOfWork _UnityOfWork;
+
+
+        public EmpleadoesController(IUnityOfWork unityOfWork)
+        {
+            _UnityOfWork = unityOfWork;
+        }
+        // GET: Empleadoes
+        public ActionResult Index()
+        {
+            return View(_UnityOfWork.Empleados.GetAll());
+        }
+
+        // GET: Empleadoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = _UnityOfWork.Empleados.Get(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleadoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empleadoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "empleadoId,nombre,apellidos,direccion,celular")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                _UnityOfWork.Empleados.Add(empleado);
+                _UnityOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empleado);
+        }
+
+        // GET: Empleadoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = _UnityOfWork.Empleados.Get(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleadoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "empleadoId,nombre,apellidos,direccion,celular")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                _UnityOfWork.StateModified(empleado);
+                _UnityOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleadoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = _UnityOfWork.Empleados.Get (id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleadoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleado empleado = _UnityOfWork.Empleados.Get(id);
+            _UnityOfWork.Empleados.Remove(empleado);
+            _UnityOfWork.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _UnityOfWork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

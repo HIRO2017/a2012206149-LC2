d1e39dbb09741dff2130102dfb9713342f6ac6e8
Hiroshi Miyasato Reyes <hiroshi_miyasato@usmp.pe>
2017-06-10 14:20:37 -0500

    se crearon 2 apis

---
+++ a12206149-SLN/a12206149.API/Controllers/AdministrativoAPIController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using a12206149_ENT.Entities;
+using a12206149_PER;
+
+namespace a12206149.API.Controllers
+{
+    public class AdministrativoAPIController : ApiController
+    {
+        private a12206149DbContext db = new a12206149DbContext();
+
+        // GET: api/AdministrativoAPI
+        public IQueryable<Administrativo> GetAdministrativos()
+        {
+            return db.Administrativos;
+        }
+
+        // GET: api/AdministrativoAPI/5
+        [ResponseType(typeof(Administrativo))]
+        public IHttpActionResult GetAdministrativo(int id)
+        {
+            Administrativo administrativo = db.Administrativos.Find(id);
+            if (administrativo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(administrativo);
+        }
+
+        // PUT: api/AdministrativoAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAdministrativo(int id, Administrativo administrativo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != administrativo.administrativoId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(administrativo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdministrativoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/AdministrativoAPI
+        [ResponseType(typeof(Administrativo))]
+        public IHttpActionResult PostAdministrativo(Administrativo administrativo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Administrativos.Add(administrativo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = administrativo.administrativoId }, administrativo);
+        }
+
+        // DELETE: api/AdministrativoAPI/5
+        [ResponseType(typeof(Administrativo))]
+        public IHttpActionResult DeleteAdministrativo(int id)
+        {
+            Administrativo administrativo = db.Administrativos.Find(id);
+            if (administrativo == null)
+            {
+                return NotFound();
+            }
+
+            db.Administrativos.Remove(administrativo);
+            db.SaveChanges();
+
+            return Ok(administrativo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AdministrativoExists(int id)
+        {
+            return db.Administrativos.Count(e => e.administrativoId == id) > 0;
+        }
+    }
+}
---
+++ a12206149-SLN/a12206149.API/Controllers/BusesAPIController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using a12206149_ENT.Entities;
+using a12206149_PER;
+
+namespace a12206149.API.Controllers
+{
+    public class BusesAPIController : ApiController
+    {
+        private a12206149DbContext db = new a12206149DbContext();
+
+        // GET: api/BusesAPI
+        public IQueryable<Bus> GetBuses()
+        {
+            return db.Buses;
+        }
+
+        // GET: api/BusesAPI/5
+        [ResponseType(typeof(Bus))]
+        public IHttpActionResult GetBus(int id)
+        {
+            Bus bus = db.Buses.Find(id);
+            if (bus == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bus);
+        }
+
+        // PUT: api/BusesAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBus(int id, Bus bus)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bus.busId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(bus).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/BusesAPI
+        [ResponseType(typeof(Bus))]
+        public IHttpActionResult PostBus(Bus bus)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Buses.Add(bus);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = bus.busId }, bus);
+        }
+
+        // DELETE: api/BusesAPI/5
+        [ResponseType(typeof(Bus))]
+        public IHttpActionResult DeleteBus(int id)
+        {
+            Bus bus = db.Buses.Find(id);
+            if (bus == null)
+            {
+                return NotFound();
+            }
+
+            db.Buses.Remove(bus);
+            db.SaveChanges();
+
+            return Ok(bus);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BusExists(int id)
+        {
+            return db.Buses.Count(e => e.busId == id) > 0;
+        }
+    }
+}

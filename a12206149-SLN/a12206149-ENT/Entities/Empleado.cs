9a72d80344ddd59f3d199f7c039ceda7e0ca3884
Hiroshi Miyasato <hiroshi_miyasato@usmp.pe>
2017-06-09 21:06:23 -0500

    se agrego entities

--- a12206149-SLN/a12206149-ENT/Entities/Empleado.cs
+++ a12206149-SLN/a12206149-ENT/Entities/Empleado.cs
@@ -4,14 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace a12206149_ENT
+namespace a12206149_ENT.Entities
 {
-    public class Empleado
+    class Empleado
     {
-        public int empleadoId { get; set; }
-        public string nombre { get; set; }
-        public string apellidos { get; set; }
-        public string direccion { get; set; }
-        public int celular { get; set; }
     }
 }

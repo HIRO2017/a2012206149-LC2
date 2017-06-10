8a3e1554b7e1439a0a621ade2513be463159ce74
Hiroshi Miyasato <hiroshi_miyasato@usmp.pe>
2017-06-09 23:32:40 -0500

    creacion de entityTypeConfiguration

---
+++ a12206149-SLN/a12206149-PER/EntityTypeConfiguration/ServicioConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using a12206149_ENT.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a12206149_PER.EntityTypeConfiguration
+{
+    public class ServicioConfiguration : EntityTypeConfiguration<Servicio>
+    {
+    }
+}
